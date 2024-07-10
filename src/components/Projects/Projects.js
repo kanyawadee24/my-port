7a3d2dc8fe29b55dcffc8aad70e3311ccb258ce7
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-07-10 20:02:01 +0700

    done

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -33,8 +33,7 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="one day react project"
-              description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi voluptatum modi tenetur. Possimus dicta beatae omnis dolores odio amet commodi? Quasi voluptatum modi tenetur.  Modi sunt nesciunt itaque natus!
- "
+              description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi voluptatum modi tenetur. Possimus dicta beatae omnis dolores odio amet commodi? Quasi voluptatum modi tenetur.  dolor sit amet consectetur adipisicing elit.  Modi sunt nesciunt itaque natus!"
               ghLink="https://github.com/kanyawadee24/12-party-react"
               demoLink="https://12-party-react.vercel.app/"
             />
@@ -44,7 +43,5 @@ function Projects() {
     </Container>
   );
 }
-
-
 
 export default Projects;
