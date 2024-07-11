baab558cdc07d5f9dcee103451a92d9ddf9881e1
Kanya.wd <117735915+kanyawadee24@users.noreply.github.com>
2024-07-11 10:40:17 +0700

    finish portfolio

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import chatify from "../../Assets/Projects/chatify.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import potioncafe from "../../Assets/Projects/potioncafe.png";
 
 function Projects() {
   return (
@@ -19,25 +18,15 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={potioncafe}
               isBlog={false}
               title="PotionCafe"
               description="Project presents a fully functional e-commerce platform tailored for a coffee shop, aimed at enhancing customer experience and optimizing online sales. With a user-friendly interface, it allows coffee enthusiasts to explore diverse products, securely place orders, Developed using modern web technologies "
               ghLink="https://github.com"
-              demoLink="l"
+              demoLink="test"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="one day react project"
-              description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi voluptatum modi tenetur. Possimus dicta beatae omnis dolores odio amet commodi? Quasi voluptatum modi tenetur.  dolor sit amet consectetur adipisicing elit.  Modi sunt nesciunt itaque natus!"
-              ghLink="https://github.com/kanyawadee24/12-party-react"
-              demoLink="https://12-party-react.vercel.app/"
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
